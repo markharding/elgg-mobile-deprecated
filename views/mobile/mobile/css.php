317995540b8178e66f286b8aebfd90eaeebd4b8b
Mark Harding <mark@marksmusiconline.co.uk>
2012-07-08 16:16:34 +0100

    git ignore now works

---
+++ views/mobile/mobile/css.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Specific css for the elgg mobile plugin
+ * written by Mark Harding @ kramnorth
+ * copyright Kramnorth 2011
+ */
+?>
+
+.logo_centre{
+margin:auto;
+width:100%;
+height:50px;
+}
+img.logo_centre{
+height:22px;
+width:42px;
+ display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.elgg-menu-extras-default{
+display:none;
+}
+.elgg-header{
+	background-color:#999;
+    }
+.elgg-header .ui-btn-corner-all {
+	-moz-border-radius: 				0 /*{global-radii-buttons}*/;
+	-webkit-border-radius: 			0 /*{global-radii-buttons}*/;
+	border-radius: 					0 /*{global-radii-buttons}*/;
+    }
+    
+/*INPUT FIELD AND BUTTON MODS */
+fieldset > div {
+	margin-bottom: 15px;
+    padding-left:10px;
+}
